4c9b04ef163d6361519c063e5c47d14fb4c09deb
Solopova Olga <olga.solopova21@gmail.com>
2025-08-02 21:33:04 +0300

    added client/, page.tsx, Navbar.tsx

---
+++ client/app/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-100 to-white">
+        <h1 className="text-4xl font-bold text-blue-700 mb-4">Welcome to music-platform</h1>
+        <h3 className="text-xl text-gray-700 mb-2">The best tracks are collected here!</h3>
+        <p className="text-base text-gray-500">Explore our collection of music tracks and enjoy the rhythm!</p>
+      </div>
+    </>
+  );
+}
